73cdb9e6a80b7a8c82131890d70d8d76588a5ed3
PatrickC <pat31718@yahoo.com.tw>
2016-10-24 06:58:53 +0800

    新增收集以及加上碰撞
    地圖中暫定蛋糕為跳躍用物件 甜甜圈為收集物件，可測試效果

---
+++ Assets/RunGame/Scripts/CollectAndJump.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectAndJump : MonoBehaviour {
+    private Rigidbody rb;
+    public float power;
+    private int amount;//要收集的數量
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        amount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Jump")
+        {
+            rb.velocity += Vector3.up * power;
+        }
+
+        if (other.tag == "food"){
+            amount++;
+            other.gameObject.SetActive(false);//將食物物件從遊戲中移除
+        }
+    }
+}
